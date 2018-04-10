7d10b91675dd83d62fa9ce79cb362deeeef7947e
Zihao <zi5253hao@gmail.com>
2018-04-09 18:57:22 -0700

    setting up skeleton

---
+++ server/models/userModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose"); 
+const Schema = mongoose.Schema; 
+
+const userSchema = new Schema({
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    email: {type: String, required: true},
+    phone: {type: String},
+    createdAt: {type: Date, default: Date.now, required: true}
+});
+
+module.exports = mongoose.model("Users", userSchema); 
